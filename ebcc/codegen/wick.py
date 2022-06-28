afeae9b1202dd0a536217f01835de4fcbd1feeb2
Ollie Backhouse <olbackhouse@gmail.com>
2022-06-28 11:12:00 +0100

    Fixes to symmetry indices

--- ebcc/codegen/wick.py
+++ ebcc/codegen/wick.py
@@ -262,7 +262,7 @@ def get_r_ip_spaces(rank=(2, 0, 0), occs=None, virs=None, nms=None):
         scalar = get_factor(*occ, *vir)
         sums = [Sigma(i) for i in occ] + [Sigma(a) for a in vir]
         operators = [FOperator(a, True) for a in vir] + [FOperator(i, False) for i in occ[::-1]]
-        tensors = [Tensor(occ + vir, name, sym=TensorSym([[0, 1, 2], [1, 0, 2]], [1, 1]) if n == 2 else TensorSym([], []))]  # FIXME symm
+        tensors = [Tensor(occ + vir, name, sym=TensorSym([[0, 1, 2], [1, 0, 2]], [1, -1]) if n == 2 else TensorSym([], []))]  # FIXME symm
         rs.append(Expression([Term(scalar, sums, tensors, operators, [])]))
 
     # boson
@@ -290,7 +290,7 @@ def get_r_ea_spaces(rank=(2, 0, 0), occs=None, virs=None, nms=None):
         scalar = get_factor(*occ, *vir)
         sums = [Sigma(a) for a in vir] + [Sigma(i) for i in occ]
         operators = [FOperator(a, True) for a in vir[::-1]] + [FOperator(i, False) for i in occ]
-        tensors = [Tensor(occ + vir, name, sym=TensorSym([[0, 1, 2], [1, 0, 2]], [1, 1]) if n == 2 else TensorSym([], []))]  # FIXME symm
+        tensors = [Tensor(occ + vir, name, sym=TensorSym([[0, 1, 2], [0, 2, 1]], [1, -1]) if n == 2 else TensorSym([], []))]  # FIXME symm
         rs.append(Expression([Term(scalar, sums, tensors, operators, [])]))
 
     # boson
