dbac39f0de06cd0aab2fd7c45640c4fe9a66fb73
Ollie Backhouse <olbackhouse@gmail.com>
2022-07-07 18:34:34 +0100

    Adds CCSD-S-S tests (RDMs failing)

---
+++ tests/test_RCCSD_S_S.py
@@ -0,0 +1,142 @@
+"""Tests for the RCCSD-S-S model.
+"""
+
+import unittest
+import pytest
+import itertools
+import pickle
+import os
+
+import numpy as np
+import scipy.linalg
+
+from pyscf import gto, scf, cc, lib
+
+from ebcc import util, REBCC
+
+
+class RCCSD_S_S_Tests(unittest.TestCase):
+    shift = True
+
+    @classmethod
+    def setUpClass(cls):
+        path = os.path.join(os.path.dirname(os.path.realpath(__file__)), "test_data.pkl")
+        with open(path, "rb") as f:
+            data = pickle.load(f)
+            mo_coeff = data["mo_coeff"]
+            data = data[(2, 1, 1)]
+
+        mol = gto.Mole()
+        mol.atom = "H 0 0 0; F 0 0 1.1"
+        mol.basis = "6-31g"
+        mol.verbose = 0
+        mol.build()
+
+        mf = scf.RHF(mol)
+        mf.conv_tol = 1e-12
+        mf.kernel()
+        mf.mo_coeff = mo_coeff
+
+        nmo = mf.mo_occ.size
+        nbos = 5
+        np.random.seed(12345)
+        g = np.random.random((nbos, nmo, nmo)) * 0.02
+        g = 0.5 * (g + g.transpose(0, 2, 1).conj())
+        omega = np.random.random((nbos,)) * 5.0
+
+        ccsd = REBCC(mf, rank=("SD", "S", "S"), g=g, omega=omega, shift=cls.shift, log=util.NullLogger())
+        ccsd.options.e_tol = 1e-12
+        ccsd.options.t_tol = 1e-12
+        eris = ccsd.get_eris()
+        ccsd.kernel(eris=eris)
+        ccsd.solve_lambda(eris=eris)
+
+        osort = list(itertools.chain(*zip(range(ccsd.nocc), range(ccsd.nocc, 2*ccsd.nocc))))
+        vsort = list(itertools.chain(*zip(range(ccsd.nvir), range(ccsd.nvir, 2*ccsd.nvir))))
+
+        cls.mf, cls.ccsd, cls.eris, cls.data = mf, ccsd, eris, data
+        cls.osort, cls.vsort = osort, vsort
+
+    @classmethod
+    def tearDownClass(cls):
+        del cls.mf, cls.ccsd, cls.eris, cls.data
+        del cls.osort, cls.vsort
+
+    def test_const(self):
+        a = self.data[self.shift]["const"]
+        b = self.ccsd.const
+        self.assertAlmostEqual(a, b, 7)
+
+    def test_xi(self):
+        a = self.data[self.shift]["xi"]
+        b = self.ccsd.xi
+        np.testing.assert_almost_equal(a, b, 7, verbose=True)
+
+    def test_energy(self):
+        a = self.data[self.shift]["e_corr"]
+        b = self.ccsd.e_corr
+        self.assertAlmostEqual(a, b, 7)
+
+    def test_t1_amplitudes(self):
+        a = self.data[self.shift]["t1"]
+        b = scipy.linalg.block_diag(self.ccsd.t1, self.ccsd.t1)[self.osort][:, self.vsort]
+        np.testing.assert_almost_equal(a, b, 6, verbose=True)
+
+    def test_s1_amplitudes(self):
+        a = self.data[self.shift]["s1"]
+        b = self.ccsd.amplitudes["s1"]
+        np.testing.assert_almost_equal(a, b, 6, verbose=True)
+
+    def test_u11_amplitudes(self):
+        a = self.data[self.shift]["u11"]
+        b = np.array([scipy.linalg.block_diag(x, x) for x in self.ccsd.amplitudes["u11"]])
+        b = b[:, self.osort][:, :, self.vsort]
+        np.testing.assert_almost_equal(a, b, 6, verbose=True)
+
+    def test_l1_amplitudes(self):
+        a = self.data[self.shift]["l1"]
+        b = scipy.linalg.block_diag(self.ccsd.l1, self.ccsd.l1)[self.vsort][:, self.osort]
+        np.testing.assert_almost_equal(a, b, 6, verbose=True)
+
+    def test_ls1_amplitudes(self):
+        a = self.data[self.shift]["ls1"]
+        b = self.ccsd.lambdas["ls1"]
+        np.testing.assert_almost_equal(a, b, 6, verbose=True)
+
+    def test_lu11_amplitudes(self):
+        a = self.data[self.shift]["lu11"]
+        b = np.array([scipy.linalg.block_diag(x, x) for x in self.ccsd.lambdas["lu11"]])
+        b = b[:, self.vsort][:, :, self.osort]
+        np.testing.assert_almost_equal(a, b, 6, verbose=True)
+
+    def test_rdm1_f(self):
+        rdm1_f = self.ccsd.make_rdm1_f()
+        rdm1_f = 0.5 * (rdm1_f + rdm1_f.T.conj())
+        a = self.data[self.shift]["rdm1_f"]
+        b = scipy.linalg.block_diag(rdm1_f, rdm1_f) / 2
+        np.testing.assert_almost_equal(a, b, 6, verbose=True)
+
+    def test_rdm1_b(self):
+        a = self.data[self.shift]["rdm1_b"]
+        b = self.ccsd.make_rdm1_b()
+        np.testing.assert_almost_equal(a, b, 6, verbose=True)
+
+    def test_dm_b(self):
+        a = self.data[self.shift]["dm_b"]
+        b = np.array(self.ccsd.make_sing_b_dm())
+        np.testing.assert_almost_equal(a, b, 6, verbose=True)
+
+    def test_rdm_eb(self):
+        a = self.data[self.shift]["rdm_eb"]
+        b = self.ccsd.make_eb_coup_rdm()
+        np.testing.assert_almost_equal(a, b, 6, verbose=True)
+
+
+class RCCSD_S_S_NoShift_Tests(RCCSD_S_S_Tests):
+    shift = False
+
+
+
+if __name__ == "__main__":
+    print("Tests for RCCSD-S-S")
+    unittest.main()
