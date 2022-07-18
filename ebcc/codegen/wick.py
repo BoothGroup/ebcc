c6c902f443580fdfe13454a0bea66016355ffd96
Ollie Backhouse <olbackhouse@gmail.com>
2022-07-18 14:51:33 +0100

    Revert symmetry

--- ebcc/codegen/wick.py
+++ ebcc/codegen/wick.py
@@ -285,8 +285,7 @@ def get_r_ip_spaces(rank=("SD", "", ""), occs=None, virs=None, nms=None):
         scalar = get_factor(*occ, *vir)
         sums = [Sigma(i) for i in occ] + [Sigma(a) for a in vir]
         operators = [FOperator(a, True) for a in vir] + [FOperator(i, False) for i in occ[::-1]]
-        #tensors = [Tensor(occ + vir, name, sym=TensorSym([[0, 1, 2], [1, 0, 2]], [1, -1]) if n == 2 else TensorSym([], []))]  # FIXME symm
-        tensors = [Tensor(occ + vir, name)]
+        tensors = [Tensor(occ + vir, name, sym=TensorSym([[0, 1, 2], [1, 0, 2]], [1, -1]) if n == 2 else TensorSym([], []))]  # FIXME symm
         rs.append(Expression([Term(scalar, sums, tensors, operators, [])]))
 
     # boson
@@ -315,8 +314,7 @@ def get_r_ea_spaces(rank=("SD", "", ""), occs=None, virs=None, nms=None):
         scalar = get_factor(*occ, *vir)
         sums = [Sigma(a) for a in vir] + [Sigma(i) for i in occ]
         operators = [FOperator(a, True) for a in vir[::-1]] + [FOperator(i, False) for i in occ]
-        #tensors = [Tensor(vir + occ, name, sym=TensorSym([[0, 1, 2], [1, 0, 2]], [1, -1]) if n == 2 else TensorSym([], []))]  # FIXME symm
-        tensors = [Tensor(vir + occ, name)]
+        tensors = [Tensor(vir + occ, name, sym=TensorSym([[0, 1, 2], [1, 0, 2]], [1, -1]) if n == 2 else TensorSym([], []))]  # FIXME symm
         rs.append(Expression([Term(scalar, sums, tensors, operators, [])]))
 
     # boson
