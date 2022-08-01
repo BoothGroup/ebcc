2b4f4bcf65e2ca9d399421a328083705e6d2de16
Ollie Backhouse <olbackhouse@gmail.com>
2022-08-01 12:17:43 +0100

    Update examples

--- examples/00-simple_ccsd.py
+++ examples/00-simple_ccsd.py
@@ -1,24 +1,15 @@
 import numpy as np
-import pyscf
-from pyscf import ao2mo
+from pyscf import gto, scf
 from ebcc import EBCC
 
-mol = pyscf.M(
-    atom = 'H 0 0 0; F 0 0 1.1',
-    basis = 'ccpvdz')
+mol = gto.Mole()
+mol.atom = "H 0 0 0; F 0 0 1.1"
+mol.basis = "cc-pvdz"
+mol.build()
 
-mf = mol.RHF().run()
+mf = scf.RHF(mol)
+mf.kernel()
 
-mycc = mf.CCSD().run()
-print('CCSD correlation energy', mycc.e_corr)
-
-ebccsd = EBCC(mf, rank=(2, 0, 0))
-ecorr = ebccsd.kernel()
-print('EBCCSD correlation energy', ebccsd.e_corr)
-
-if np.allclose(ebccsd.e_corr, mycc.e_corr):
-    print('**********************************************************************')
-    print('EXCELLENT: CCSD correlation energies agree between pyscf and ebcc')
-    print('**********************************************************************')
-else:
-    print('pyscf and ebcc correlation energies do not agree...')
+ccsd = EBCC(mf)
+ccsd.kernel()
+print("CCSD correlation energy:", ccsd.e_corr)
--- examples/01-simple_ebcc.py
+++ examples/01-simple_ebcc.py
@@ -34,3 +34,36 @@ print('EBCCSD correlation energy for rank 221:   {}'.format(e_corr))
 cc = EBCC(myhf, rank=(2, 2, 2), omega=omega, g=g)
 e_corr = cc.kernel()
 print('EBCCSD correlation energy for rank 222:   {}'.format(e_corr))
+
+import numpy as np
+from pyscf import gto, scf
+from ebcc import EBCC
+
+mol = gto.Mole()
+mol.atom = "H 0 0 0; F 0 0 1.1"
+mol.basis = "cc-pvdz"
+mol.build()
+
+mf = scf.RHF(mol)
+mf.kernel()
+
+# Define boson energies and couplings to AO density:
+nbos = 5
+nmo = mf.mo_occ.size
+g = np.random.random((nbos, nmo, nmo)) * 0.03
+omega = np.random.random((nbos,)) * 5.0
+
+# One-boson amplitudes and one-boson-one-fermion coupling:
+ccsd = EBCC(mf, rank=("SD", "S", "S"), omega=omega, g=g)
+ccsd.kernel()
+print("%s correlation energy:" % ccsd.name, ccsd.e_corr)
+
+# Include two-boson amplitudes:
+ccsd = EBCC(mf, rank=("SD", "SD", "S"), omega=omega, g=g)
+ccsd.kernel()
+print("%s correlation energy:" % ccsd.name, ccsd.e_corr)
+
+# Include two-boson-one-fermion coupling:
+ccsd = EBCC(mf, rank=("SD", "SD", "SD"), omega=omega, g=g)
+ccsd.kernel()
+print("%s correlation energy:" % ccsd.name, ccsd.e_corr)
