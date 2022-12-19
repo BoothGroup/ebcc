c4aa08b47404f593a198952123aa3468c000e1b1
Ollie Backhouse <olbackhouse@gmail.com>
2022-12-19 19:51:51 +0000

    Adds GCCSDT amplitudes

---
+++ tests/test_GCCSDT.py
@@ -0,0 +1,50 @@
+"""Tests for the GCCSDT model.
+"""
+
+import unittest
+
+import numpy as np
+import pytest
+from pyscf import gto, lib, scf, fci
+
+from ebcc import GEBCC, NullLogger
+
+# TODO from http://dx.doi.org/10.1021/acs.jpca.7b10892
+
+
+class GCCSDT_Tests(unittest.TestCase):
+    """Test GCCSDT against regression.
+    """
+
+    def test_3_electron_exact(self):
+        mol = gto.M(
+                atom="H 0 0 0; H 0 0 1",
+                basis="6-31g",
+                spin=1,
+                charge=-1,
+                verbose=0,
+        )
+        assert mol.nelectron == 3
+
+        mf = scf.UHF(mol)
+        mf.kernel()
+
+        ccsdt = GEBCC(
+                mf,
+                ansatz="CCSDT",
+                e_tol=1e-12,
+                log=NullLogger(),
+        )
+        ccsdt.kernel()
+        e1 = ccsdt.e_tot
+
+        ci = fci.FCI(mf)
+        ci.conv_tol = 1e-12
+        e2 = ci.kernel()[0]
+
+        self.assertAlmostEqual(e1, e2, 6)
+
+
+if __name__ == "__main__":
+    print("Tests for GCCSDT")
+    unittest.main()
