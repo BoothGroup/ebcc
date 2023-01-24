e24e2e6cf24e8f1a074c4eeb549aa4e23dd59056
Ollie Backhouse <olbackhouse@gmail.com>
2023-01-24 20:57:17 +0000

    T1 tests

--- tests/test_UCCSDT.py
+++ tests/test_UCCSDT.py
@@ -1,11 +1,13 @@
 """Tests for the UCCSDT model.
 """
 
+import itertools
 import unittest
 
 import numpy as np
 import pytest
 from pyscf import gto, lib, scf, fci
+import scipy.linalg
 
 from ebcc import UEBCC, GEBCC, NullLogger
 
@@ -34,8 +36,8 @@ class UCCSDT_Tests(unittest.TestCase):
                 ansatz="CCSDT",
                 log=NullLogger(),
         )
-        uccsdt.options.e_tol = 1e-8
-        uccsdt.options.t_tol = 1e-6
+        uccsdt.options.e_tol = 1e-10
+        uccsdt.options.t_tol = 1e-8
         uccsdt.kernel()
 
         gccsdt = GEBCC(
@@ -43,15 +45,20 @@ class UCCSDT_Tests(unittest.TestCase):
                 ansatz="CCSDT",
                 log=NullLogger(),
         )
-        gccsdt.options.e_tol = 1e-8
-        gccsdt.options.t_tol = 1e-6
+        gccsdt.options.e_tol = 1e-10
+        gccsdt.options.t_tol = 1e-8
         gccsdt.kernel()
 
+        osort = list(itertools.chain(*zip(range(uccsdt.nocc[0], uccsdt.nocc[0]+uccsdt.nocc[1]), range(uccsdt.nocc[0]))))
+        vsort = list(itertools.chain(*zip(range(uccsdt.nvir[0], uccsdt.nvir[0]+uccsdt.nvir[1]), range(uccsdt.nvir[1]))))
+
         cls.mf, cls.uccsdt, cls.gccsdt = mf, uccsdt, gccsdt
+        cls.osort, cls.vsort = osort, vsort
 
     @classmethod
     def tearDownClass(cls):
         del cls.mf, cls.uccsdt, cls.gccsdt
+        del cls.osort, cls.vsort
 
     def test_energy(self):
         a = self.uccsdt.e_tot
@@ -84,6 +91,11 @@ class UCCSDT_Tests(unittest.TestCase):
         e2 = ci.kernel()[0]
 
         self.assertAlmostEqual(e1, e2, 6)
+
+    def test_t1(self):
+        a = scipy.linalg.block_diag(self.uccsdt.t1.aa, self.uccsdt.t1.bb)[self.osort][:, self.vsort]
+        b = self.gccsdt.t1
+        np.testing.assert_almost_equal(a, b, 6)
 
 
 if __name__ == "__main__":
