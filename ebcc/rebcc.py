a85c4c2bb04892a7e916a7a6e182dab3064f88bd
Ollie Backhouse <olbackhouse@gmail.com>
2023-02-06 17:17:14 +0000

    Fix again

--- ebcc/rebcc.py
+++ ebcc/rebcc.py
@@ -93,7 +93,7 @@ class ERIs(types.SimpleNamespace):
             for i, k in enumerate(key):
                 slices.append(self.slices[i][k])
             si, sj, sk, sl = slices
-            block = self.array[si, sj, sk, sl]
+            block = self.array[si][:, sj][:, :, sk][:, :, :, sl]
             return block
 
 
