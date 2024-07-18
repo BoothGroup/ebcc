b2adb589653ad0b3c49ef4abdc277848448091e5
Oliver Backhouse <olbackhouse@gmail.com>
2024-07-18 22:16:57 +0100

    Improve bootstrapping script

--- .github/workflows/bootstrap_all.sh
+++ .github/workflows/bootstrap_all.sh
@@ -1,16 +1,17 @@
 #!/bin/bash
-for i in rhf uhf ghf; do
-    for n in 2 3; do
-        python -W ignore bootstrap_MPn.py $i $n
+for method in MPn CCD CCSD QCISD CC2 DCD DCSD; do
+    for i in rhf uhf ghf; do
+        for n in 2 3; do
+            echo "Bootstrapping $method ($i) (order=$n)"
+            python -W ignore bootstrap_${method}.py $i $n
+        done
+        echo "Bootstrapping $method ($i)"
+        python -W ignore bootstrap_${method}.py $i
     done
-    python -W ignore bootstrap_CCD.py $i
-    python -W ignore bootstrap_CCSD.py $i
-    python -W ignore bootstrap_QCISD.py $i
-    python -W ignore bootstrap_CC2.py $i
-    python -W ignore bootstrap_DCD.py $i
-    python -W ignore bootstrap_DCSD.py $i
 done
-for i in rhf uhf; do
-    python -W ignore bootstrap_DFCCD.py $i
-    python -W ignore bootstrap_DFCCSD.py $i
+for method in DFCCD DFCCSD; do
+    for i in rhf uhf; do
+        echo "Bootstrapping $method ($i)"
+        python -W ignore bootstrap_${method}.py $i
+    done
 done
