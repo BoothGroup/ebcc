7296f1580554977116ba993b10bad559f71cafb3
Oliver Backhouse <olbackhouse@gmail.com>
2024-08-09 23:57:38 +0100

    Bump version to 1.5.0

--- ebcc/__init__.py
+++ ebcc/__init__.py
@@ -37,7 +37,7 @@ The implemented models are built upon the mean-field objects of
 from __future__ import annotations
 
 """Version of the package."""
-__version__ = "1.4.5"
+__version__ = "1.5.0"
 
 """List of supported ansatz types."""
 METHOD_TYPES = ["MP", "CC", "LCC", "QCI", "QCC", "DC"]
