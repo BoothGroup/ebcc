83feb07ead60a393c9605cb9441084ad41339b13
Oliver Backhouse <olbackhouse@gmail.com>
2024-08-09 23:48:52 +0100

    Working on MP2 DMs, broken

--- ebcc/codegen/bootstrap_MPn.py
+++ ebcc/codegen/bootstrap_MPn.py
@@ -87,6 +87,138 @@ with Stopwatch("Energy"):
             expr,
         )
 
+if order == 2:
+    pq = pdaggerq.pq_helper("fermi")
+    pq.set_print_level(0)
+
+    with Stopwatch("1RDM"):
+        # Get the 1RDM contractions in pdaggerq format
+        terms = {
+            ("oo", "ij"): [["+1.0", "d(i,j)"], ["-0.5", "t2(b,a,k,i)", "l2(k,j,b,a)"]],
+            ("vv", "ab"): [["+0.5", "t2(b,c,i,j)", "l2(i,j,a,c)"]]
+        }
+
+        # Get the 1RDM in albert format
+        expr = []
+        output = []
+        returns = []
+        for sectors, indices in [("oo", "ij"), ("ov", "ia"), ("vo", "ai"), ("vv", "ab")]:
+            if (sectors, indices) not in terms:
+                continue
+            for index_spins in get_density_spins(1, spin, indices):
+                expr_n = import_from_pdaggerq(terms[sectors, indices], index_spins=index_spins)
+                if not (isinstance(expr_n, int) and expr_n == 0):
+                    expr_n = spin_integrate(expr_n, spin)
+                    if spin == "rhf":
+                        expr_n = tuple(e * 2 for e in expr_n)
+                    output_n = get_density_outputs(expr_n, f"d", indices)
+                    returns_n = (Tensor(*tuple(Index(i, index_spins[i], space=s) for i, s in zip(indices, sectors)), name=f"d"),)
+                    expr.extend(expr_n)
+                    output.extend(output_n)
+                    returns.extend(returns_n)
+        output, expr = optimise(output, expr, spin, strategy="exhaust")
+
+        # Generate the 1RDM code
+        for name, codegen in code_generators.items():
+            if name == "einsum":
+                def get_postamble(n, spin, name="rdm{n}"):
+                    nm = name.format(n=n)
+                    postamble = ""
+                    if spin != "uhf":
+                        for occ in ("ov", "vo"):
+                            shape = ", ".join(f"t2.shape[{'0' if o == 'o' else '-1'}]" for o in occ)
+                            postamble += f"{nm}.{occ} = np.zeros(({shape}))\n"
+                    else:
+                        for s in "ab":
+                            for occ in ("ov", "vo"):
+                                shape = ", ".join(f"t2.{s}{s}{s}{s}.shape[{'0' if o == 'o' else '-1'}]" for o in occ)
+                                postamble += f"{nm}.{s}{s}.{occ} = np.zeros(({shape}))\n"
+                    return postamble + get_density_einsum_postamble(n, spin)
+                kwargs = {
+                    "preamble": get_density_einsum_preamble(1, spin),
+                    "postamble": get_postamble(1, spin),
+                }
+            else:
+                kwargs = {}
+            codegen(
+                "make_rdm1_f",
+                returns,
+                output,
+                expr,
+                **kwargs,
+            )
+
+    with Stopwatch("2RDM"):
+        # Get the 2RDM contractions in pdaggerq format
+        terms = {
+            ('oooo', 'ijkl'): [['+1.0', 'P(i,j)', 'd(i,k)', 'd(j,l)']],
+            ('oovv', 'ijab'): [['+0.25', 't2(a,b,i,j)']],
+            ('vvoo', 'abij'): [['+0.25', 'l2(a,b,i,j)']],
+        }
+
+        # Get the 2RDM in albert format
+        expr = []
+        output = []
+        returns = []
+        for sectors, indices in [
+            ("oooo", "ijkl"),
+            ("ooov", "ijka"),
+            ("oovo", "ijak"),
+            ("ovoo", "iajk"),
+            ("vooo", "aijk"),
+            ("oovv", "ijab"),
+            ("ovov", "iajb"),
+            ("ovvo", "iabj"),
+            ("voov", "aijb"),
+            ("vovo", "aibj"),
+            ("vvoo", "abij"),
+            ("ovvv", "iabc"),
+            ("vovv", "aibc"),
+            ("vvov", "abic"),
+            ("vvvo", "abci"),
+            ("vvvv", "abcd"),
+        ]:
+            if (sectors, indices) not in terms:
+                continue
+            for index_spins in get_density_spins(2, spin, indices):
+                expr_n = import_from_pdaggerq(terms[sectors, indices], index_spins=index_spins)
+                if not (isinstance(expr_n, int) and expr_n == 0):
+                    expr_n = spin_integrate(expr_n, spin)
+                    output_n = get_density_outputs(expr_n, f"Γ", indices)
+                    returns_n = (Tensor(*tuple(Index(i, index_spins[i], space=s) for i, s in zip(indices, sectors)), name=f"Γ"),)
+                    expr.extend(expr_n)
+                    output.extend(output_n)
+                    returns.extend(returns_n)
+        output, expr = optimise(output, expr, spin, strategy="exhaust")
+
+        # Generate the 2RDM code
+        for name, codegen in code_generators.items():
+            if name == "einsum":
+                def get_postamble(n, spin, name="rdm{n}"):
+                    nm = name.format(n=n)
+                    postamble = ""
+                    if spin != "uhf":
+                        for occ in [k[0] for k, v in terms.items() if v]:
+                            shape = ", ".join(f"t2.shape[{'0' if o == 'o' else '-1'}]" for o in occ)
+                            postamble += f"{nm}.{occ} = np.zeros(({shape}))\n"
+                    else:
+                        for s1, s2 in [("a", "a"), ("a", "b"), ("b", "b")]:
+                            for occ in [k[0] for k, v in terms.items() if v]:
+                                shape = ", ".join(f"t2.{s}{s}{s}{s}.shape[{'0' if o == 'o' else '-1'}]" for o, s in zip(occ, s1+s2+s1+s2))
+                                postamble += f"{nm}.{s1}{s2}{s1}{s2}.{occ} = np.zeros(({shape}))\n"
+                    return postamble + get_density_einsum_postamble(n, spin)
+                kwargs = {
+                    "preamble": get_density_einsum_preamble(2, spin),
+                    "postamble": get_postamble(2, spin),
+                }
+            codegen(
+                "make_rdm2_f",
+                returns,
+                output,
+                expr,
+                **kwargs,
+            )
+
 for codegen in code_generators.values():
     codegen.postamble()
     codegen.stdout.close()
