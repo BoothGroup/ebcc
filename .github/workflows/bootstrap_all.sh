e8e9c9765f6179d11132595dc7fa48ac06869e51
Oliver Backhouse <olbackhouse@gmail.com>
2024-10-18 16:50:32 +0100

    Report times clearly

--- .github/workflows/bootstrap_all.sh
+++ .github/workflows/bootstrap_all.sh
@@ -4,7 +4,7 @@ for method in MPn; do
     for n in 2 3; do
         for i in rhf uhf ghf; do
             echo "Bootstrapping $method ($i) (order=$n)"
-            python -W ignore bootstrap_${method}.py $i $n &
+            python -W ignore bootstrap_${method}.py $i $n &> output_${method}_${i}_${n}.dat &
         done
     done
 done
@@ -12,26 +12,31 @@ done
 for method in CCD CCSD QCISD CC2 DCD DCSD; do
     for i in rhf uhf ghf; do
         echo "Bootstrapping $method ($i)"
-        python -W ignore bootstrap_${method}.py $i &
+        python -W ignore bootstrap_${method}.py $i &> output_${method}_${i}.dat &
     done
     for i in rhf uhf; do
         echo "Bootstrapping DF-$method ($i)"
-        python -W ignore bootstrap_DF${method}.py $i &
+        python -W ignore bootstrap_DF${method}.py $i &> output_DF${method}_${i}.dat &
     done
 done
 
 for method in CCSDwtwp; do
     for i in rhf ghf; do
         echo "Bootstrapping $method ($i)"
-        python -W ignore bootstrap_${method}.py $i &
+        python -W ignore bootstrap_${method}.py $i &> output_${method}_${i}.dat &
     done
 done
 
 for method in CC3 CCSDT; do
     for i in rhf uhf ghf; do
         echo "Bootstrapping $method ($i)"
-        python -W ignore bootstrap_${method}.py $i &
+        python -W ignore bootstrap_${method}.py $i &> output_${method}_${i}.dat &
     done
 done
 
 wait
+
+for file in output_*.dat; do
+    echo "Output file: $file"
+    cat $file
+done
