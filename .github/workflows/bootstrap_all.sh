6fe9c5ba417c1b869015597a2bca4a614a390235
Oliver Backhouse <olbackhouse@gmail.com>
2024-10-18 16:23:35 +0100

    Run bootstrap jobs in background

--- .github/workflows/bootstrap_all.sh
+++ .github/workflows/bootstrap_all.sh
@@ -4,7 +4,7 @@ for method in MPn; do
     for n in 2 3; do
         for i in rhf uhf ghf; do
             echo "Bootstrapping $method ($i) (order=$n)"
-            python -W ignore bootstrap_${method}.py $i $n
+            python -W ignore bootstrap_${method}.py $i $n &
         done
     done
 done
@@ -12,24 +12,24 @@ done
 for method in CCD CCSD QCISD CC2 DCD DCSD; do
     for i in rhf uhf ghf; do
         echo "Bootstrapping $method ($i)"
-        python -W ignore bootstrap_${method}.py $i
+        python -W ignore bootstrap_${method}.py $i &
     done
     for i in rhf uhf; do
         echo "Bootstrapping DF-$method ($i)"
-        python -W ignore bootstrap_DF${method}.py $i
+        python -W ignore bootstrap_DF${method}.py $i &
     done
 done
 
 for method in CCSDwtwp; do
     for i in rhf ghf; do
         echo "Bootstrapping $method ($i)"
-        python -W ignore bootstrap_${method}.py $i
+        python -W ignore bootstrap_${method}.py $i &
     done
 done
 
 for method in CC3 CCSDT; do
     for i in rhf uhf ghf; do
         echo "Bootstrapping $method ($i)"
-        python -W ignore bootstrap_${method}.py $i
+        python -W ignore bootstrap_${method}.py $i &
     done
 done
