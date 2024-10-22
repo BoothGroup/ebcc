2c5f5bd59c95985ca3e1fc489b3bb6c2b6a8d318
Oliver Backhouse <olbackhouse@gmail.com>
2024-10-22 08:40:25 +0100

    We'll have to do CCSDT locally

--- .github/workflows/bootstrap_all.sh
+++ .github/workflows/bootstrap_all.sh
@@ -1,34 +1,33 @@
 #!/bin/bash
 
-#for method in MPn; do
-#    for n in 2 3; do
-#        for i in rhf uhf ghf; do
-#            echo "Bootstrapping $method ($i) (order=$n)"
-#            python -W ignore bootstrap_${method}.py $i $n &> output_${method}_${i}_${n}.dat &
-#        done
-#    done
-#done
-#
-#for method in CCD CCSD QCISD CC2 DCD DCSD; do
-#    for i in rhf uhf ghf; do
-#        echo "Bootstrapping $method ($i)"
-#        python -W ignore bootstrap_${method}.py $i &> output_${method}_${i}.dat &
-#    done
-#    for i in rhf uhf; do
-#        echo "Bootstrapping DF-$method ($i)"
-#        python -W ignore bootstrap_DF${method}.py $i &> output_DF${method}_${i}.dat &
-#    done
-#done
-#
-#for method in CCSDwtwp; do
-#    for i in rhf ghf; do
-#        echo "Bootstrapping $method ($i)"
-#        python -W ignore bootstrap_${method}.py $i &> output_${method}_${i}.dat &
-#    done
-#done
+for method in MPn; do
+    for n in 2 3; do
+        for i in rhf uhf ghf; do
+            echo "Bootstrapping $method ($i) (order=$n)"
+            python -W ignore bootstrap_${method}.py $i $n &> output_${method}_${i}_${n}.dat &
+        done
+    done
+done
 
-#for method in CC3 CCSDT; do
-for method in CCSDT; do
+for method in CCD CCSD QCISD CC2 DCD DCSD; do
+    for i in rhf uhf ghf; do
+        echo "Bootstrapping $method ($i)"
+        python -W ignore bootstrap_${method}.py $i &> output_${method}_${i}.dat &
+    done
+    for i in rhf uhf; do
+        echo "Bootstrapping DF-$method ($i)"
+        python -W ignore bootstrap_DF${method}.py $i &> output_DF${method}_${i}.dat &
+    done
+done
+
+for method in CCSDwtwp; do
+    for i in rhf ghf; do
+        echo "Bootstrapping $method ($i)"
+        python -W ignore bootstrap_${method}.py $i &> output_${method}_${i}.dat &
+    done
+done
+
+for method in CC3; do
     for i in rhf uhf ghf; do
         echo "Bootstrapping $method ($i)"
         python -W ignore bootstrap_${method}.py $i &> output_${method}_${i}.dat &
@@ -37,35 +36,34 @@ done
 
 wait
 
-#for method in MPn; do
-#    for n in 2 3; do
-#        for i in rhf uhf ghf; do
-#            echo "Output for $method ($i) (order=$n)"
-#            cat output_${method}_${i}_${n}.dat
-#        done
-#    done
-#done
-#
-#for method in CCD CCSD QCISD CC2 DCD DCSD; do
-#    for i in rhf uhf ghf; do
-#        echo "Output for $method ($i)"
-#        cat output_${method}_${i}.dat
-#    done
-#    for i in rhf uhf; do
-#        echo "Output for DF-$method ($i)"
-#        cat output_DF${method}_${i}.dat
-#    done
-#done
-#
-#for method in CCSDwtwp; do
-#    for i in rhf ghf; do
-#        echo "Output for $method ($i)"
-#        cat output_${method}_${i}.dat
-#    done
-#done
+for method in MPn; do
+    for n in 2 3; do
+        for i in rhf uhf ghf; do
+            echo "Output for $method ($i) (order=$n)"
+            cat output_${method}_${i}_${n}.dat
+        done
+    done
+done
 
-#for method in CC3 CCSDT; do
-for method in CCSDT; do
+for method in CCD CCSD QCISD CC2 DCD DCSD; do
+    for i in rhf uhf ghf; do
+        echo "Output for $method ($i)"
+        cat output_${method}_${i}.dat
+    done
+    for i in rhf uhf; do
+        echo "Output for DF-$method ($i)"
+        cat output_DF${method}_${i}.dat
+    done
+done
+
+for method in CCSDwtwp; do
+    for i in rhf ghf; do
+        echo "Output for $method ($i)"
+        cat output_${method}_${i}.dat
+    done
+done
+
+for method in CC3; do
     for i in rhf uhf ghf; do
         echo "Output for $method ($i)"
         cat output_${method}_${i}.dat
